ee40e1f825e435801ccea0f6fe8a6212ff99fa78
Michael ALTMAN <altman_mike@yahoo.fr>
2024-12-10 13:35:59 +0100

    ADDED EASYADMIN + crud interface

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Post;
+use App\Entity\Team;
+use App\Entity\User;
+use App\Entity\Video;
+use App\Entity\Member;
+use App\Entity\Product;
+use App\Entity\Innovation;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    { 
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('BodyboardBrand');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Users', 'fas fa-user', User::class);
+        yield MenuItem::linkToCrud('Posts', 'fas fa-pencil-alt', Post::class);
+        yield MenuItem::linkToCrud('Products', 'fas fa-shopping-cart', Product::class);
+        yield MenuItem::linkToCrud('Innovations','fas fa-lightbulb', Innovation::class); 
+        yield MenuItem::linkToCrud('Members','fas fa-users', Member::class); 
+        yield MenuItem::linkToCrud('Teams','fas fa-people-group', Team::class);
+        yield MenuItem::linkToCrud('Videos','fa-brands fa-youtube', Video::class);
+    }
+}
